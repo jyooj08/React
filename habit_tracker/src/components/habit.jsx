86e043ca19f239006a89f0b6c60931014c722859
Yujin Jang <jyooj08@gmail.com>
2021-11-07 19:50:52 +0900

    Add state and callback function to Habit

--- habit_tracker/src/components/habit.jsx
+++ habit_tracker/src/components/habit.jsx
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
 
 class Habit extends Component {
+    state = {
+        count: 0,
+    };
+
+    handleIncrement = event => {
+        // state 객체 안에 있는 count를 증가한 뒤 state를 업데이트 한다.
+        this.setState({count: this.state.count + 1});
+    }
+
+    handleDecrement = event => {
+        // state 객체 안에 있는 count를 감소한 뒤 state를 업데이트 한다.
+        const count = this.state.count - 1;
+        this.setState({count: count < 0 ? 0 : count});
+    }
+
     render() {
         return (
         <li className="habit">
             <span className="habit-name">Reading</span>
-            <span className="habit-count">8</span>
-            <button className="habit-button increase">
+            <span className="habit-count">{this.state.count}</span>
+            <button className="habit-button increase" onClick={this.handleIncrement}>
                 <i className="fas fa-plus-square"></i>
             </button>
-            <button className="habit-button decrease">
+            <button className="habit-button decrease" onClick={this.handleDecrement}>
                 <i className="fas fa-minus-square"></i>
             </button>
             <button className="habit-button delete">
