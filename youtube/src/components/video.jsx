4e7f9eb9484b026ab6a89a99fb4cdde11b2f7561
Yujin Jang <jyooj08@gmail.com>
2021-12-11 17:39:32 +0900

    Title text

--- youtube/src/components/video.jsx
+++ youtube/src/components/video.jsx
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 
 class Video extends Component {
+    convertText(text){
+        text.replaceAll("&#39;", "\'");
+        text.replaceAll("&quot;", "\"");
+        return text;
+    }
     render() {
         const info = this.props.info;
+        const title = info.title.replaceAll("&#39;", "\'").replaceAll("&quot;", "\"");
 
         return (
             <li className='video'>
                 <img src={info.thumbnails.default.url} alt="" />
-                <span>{info.title}</span>
+                <span>{title}</span>
             </li>
         );
     }
