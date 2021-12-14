3167997369637548051d16c40ca21c68810e0e06
Yujin Jang <jyooj08@gmail.com>
2021-12-14 21:29:23 +0900

    Make SearchHeader

--- youtube2/src/app.jsx
+++ youtube2/src/app.jsx
@@ -1,9 +1,23 @@
 import React, { useEffect, useState } from 'react';
-import './app.css';
+import styles from './app.module.css';
+import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
 
 function App() {
   const [videos, setVideos] = useState([]);
+
+  const search = (query) => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=25&q=${query}&key=AIzaSyBxhlhxZzDoUAk9lGMuICMV3JpCrAT98BY`, requestOptions)
+      .then(response => response.json())
+      .then(result => result.items.map(item => ({...item, id: item.id.videoId})))
+      .then(items => setVideos(items))
+      .catch(error => console.log('error', error));
+  };
 
   useEffect(() => {
     const requestOptions = {
@@ -19,7 +33,10 @@ function App() {
   }, []);
 
   return (
-    <VideoList videos={videos} />
+    <div className={styles.app}>
+      <SearchHeader onSearch={search}/>
+      <VideoList videos={videos} />
+    </div>
   );
 }
 
