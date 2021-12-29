eb4ac1e4e3549e98e1b1937851ede72c7fa9e006
Yujin Jang <jyooj08@gmail.com>
2021-12-30 03:20:10 +0900

    Login and Logout

--- Card_Maker2/src/app.jsx
+++ Card_Maker2/src/app.jsx
@@ -1,9 +1,16 @@
 import Login from './components/login/login';
 import styles from './app.module.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Maker from './components/maker/maker';
 
 function App({authService}) {
   return <div className={styles.app}>
-    <Login authService={authService} />
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Login authService={authService} />}></Route>
+        <Route path="/maker" element={<Maker authService={authService}></Maker>}></Route>
+      </Routes>
+    </BrowserRouter>
   </div>;
 }
 
--- Card_Maker2/src/service/auth_service.js
+++ Card_Maker2/src/service/auth_service.js
@@ -1,4 +1,4 @@
-import { getAuth, signInWithPopup, GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, GithubAuthProvider, onAuthStateChanged, signOut } from "firebase/auth";
 
 class AuthService{
     login(providerName){
@@ -9,6 +9,18 @@ class AuthService{
         else if (providerName == 'GitHub') authProvider = new GithubAuthProvider();
         
         return signInWithPopup(auth, authProvider);
+    }
+
+    logout(){
+        const auth = getAuth();
+        signOut(auth);
+    }
+
+    onAuthChange(onUserChanged){
+        const auth = getAuth();
+        onAuthStateChanged(auth, user => {
+            onUserChanged(user);
+        })
     }
 }
 
