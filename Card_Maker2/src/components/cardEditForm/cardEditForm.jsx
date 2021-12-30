129bd0cf94bf979f261b441f12b720c007e01918
Yujin Jang <jyooj08@gmail.com>
2021-12-30 17:26:26 +0900

    Editor and Preview Component UI

---
+++ Card_Maker2/src/components/cardEditForm/cardEditForm.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Button from '../button/button';
+import ImageFileInput from '../imageFileInput/imageFileInput';
+import styles from './cardEditForm.module.css';
+
+const CardEditForm = ({card}) => {
+    const {name, company, title, email, message, theme, fileName, fileURL} = card;
+
+    const onSubmit = () => {};
+    return(
+        <form className={styles.form}>
+            <input className={styles.input} type="text" name="name" value={name} />
+            <input className={styles.input} type="text" name="company" value={company} />
+            <select className={styles.select} name="theme" value={theme}>
+                <option value="light">Light</option>
+                <option value="dark">Dark</option>
+                <option value="colorful">Colorful</option>
+            </select>
+            <input className={styles.input} type="text" name="title" value={title} />
+            <input className={styles.input} type="text" name="email" value={email} />
+            <textarea className={styles.textarea} name="message" value={message}></textarea>
+            <div className={styles.fileInput}>
+                <ImageFileInput />
+            </div>
+            <div className={styles.button}>
+                <Button name='Delete' onClick={onSubmit} />
+            </div>
+            
+        </form>
+    )
+};
+
+export default CardEditForm;
